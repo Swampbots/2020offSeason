e9da98afeead9b2606c511849ec2d13de9947b52
Blake Sanders <blake.sanders10497@gmail.com>
2020-05-30 19:18:44 -0400

    Created TestOffSeason testing OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOffSeason.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test: Off-Season", group = "Testing")
+public class TestOffSeason extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addLine("Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Running");
+        telemetry.addData("Runtime", String.format("%.2f", getRuntime()));
+        telemetry.update();
+    }
+}
